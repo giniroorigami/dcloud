4b238aada9a2bd89292e32482d36ba4153ff8839
giniroorigami <koizumisouta@gmail.com>
2024-12-29 16:53:28 +0900

    Update setting.py

--- setting.py
+++ setting.py
@@ -13,7 +13,7 @@ subprocess.run(["apt","install","docker.io","-y"],text=True)
 print("docker install fin")
 
 #network
-subprocess.run("mv","/root/dcloud/50-cloud-init.yaml,"/etc/netplan/50-cloud-init.yaml",shell=True,text=True)
+subprocess.run("mv /root/dcloud/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml",shell=True,text=True)
 subprocess.run(["netplan","apply"],text=True)
 
 #reload
