4c9b1fa53d3b9dad814cbf5b53aa37e6a3dd7969
giniroorigami <koizumisouta@gmail.com>
2024-12-29 16:29:28 +0900

    Create setting.py

---
+++ setting.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import subprocess
+
+subprocess.run(["cd"],text=True)
+#hostname
+subprocess.run(["hostnamectl","set-hostname","ubuntu"],text=True)
+
+#bininstall
+subprocess.run(["apt","update","-y"],text=True)
+print("update fin")
+subprocess.run(["apt","install","nmap","-y"],text=True)
+print("nmap install fin")
+subprocess.run(["apt","install","docker.io","-y"],text=True)
+print("docker install fin")
+
+#network
+subprocess.run("cat dcloud/50-cloud-init.yaml > /etc/netplan/50-cloud-init.yaml",shell=True,text=True)
+subprocess.run(["netplan","apply"],text=True)
+
+#env
