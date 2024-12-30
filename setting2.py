d5e5500066eca059d832cbd66bb56c70bd1e58da
giniroorigami <koizumisouta@gmail.com>
2024-12-31 00:50:18 +0900

    Update setting2.py

--- setting2.py
+++ setting2.py
@@ -3,12 +3,16 @@ import subprocess
 
 #ホスト名設定
 def hostname(name): 
-  return ["hostnamectl","set-hostname",{name}]
+  return ["hostnamectl","set-hostname",name]
 
 #必要パッケージインストール
 def install(bin):
-  return ["apt","install","-y",{bin}]
+  return ["apt","install","-y",bin]
   
+#アップデート
+def update():
+    return ["apt","update","-y"]
+
 #ネットワーク設定
 def network():
   return "mv /root/dcloud/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml"
@@ -19,7 +23,8 @@ def alias():
 
 if __name__=="__main__":
   subprocess.run(hostname("ubuntu"),text=True) #ホスト名設定
+  subprocess.run(update(),text=True)
   subprocess.run(install("bind9"),text=True) #bindの追加
   subprocess.run(install("nmap"),text=True) #nmapの追加
-  subprocess.run(network(),shell=True,text=True)　#netplan設定の移設
-  subprocess.run(alias(),shell=True,text=True) #.bashrcの移設
+  subprocess.run(network(),shell=True,text=True)#netplan設定の移設
+  subprocess.run(alias(),shell=True,text=True)#.bashrcの移設
